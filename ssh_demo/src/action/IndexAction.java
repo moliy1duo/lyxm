4c3732a657a7bdcdc6e212667dcf723694b52f2b
zqq <787238097@qq.com>
2018-06-28 15:19:33 +0800

    测试提交

---
+++ ssh_demo/src/action/IndexAction.java
@@ -0,0 +1,14 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport{
+
+    @Override
+    public String execute(){
+        
+        System.out.println("该方法为默认方法被struts2自动调用");
+        
+        return "success";
+    }
+}
